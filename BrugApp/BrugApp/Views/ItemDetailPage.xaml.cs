2b7754234926b7145da69c8b1791a48ca21a4200
The_Raven <mikkel@meldgaard.net>
2021-04-28 09:23:50 +0200

    Add project files.

---
+++ BrugApp/BrugApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using BrugApp.ViewModels;
+
+namespace BrugApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
